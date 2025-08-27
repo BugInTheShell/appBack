d2095aeac7f8946deb35d777cbdcc633dcdcdc28
BugInTheShell <aboutadev@gmail.com>
2025-08-27 13:33:56 -0600

    Fix: (Correccion envio respuesta)

--- src/Routes/Files/index.ts
+++ src/Routes/Files/index.ts
@@ -46,7 +46,7 @@ router.get("/file-privileges/:id", async (req: Request, res: Response) => {
 router.post('/upload', upload.single('file'),async (req, res) => {
 
   const bucket = "almacenamiento-examen";
-  const carpeta ="Imagenes/"
+  const carpeta =`Imagenes/${req.file.originalname}`
   const url = "https://"+bucket+".s3."+process.env.AWS_REGION+".amazonaws.com/"+carpeta
 
   console.log("Carpeta a subir ",url)
